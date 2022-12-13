84053cabf96628177a4e7984606b5863210c627b
AliAban <dlsabanshaikh@gmail.com>
2022-12-13 13:06:33 +0530

    The navbar component has been made responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,18 +3,49 @@ import React, { Component } from "react";
 export default class Navbar extends Component {
   render() {
     return (
-        <nav className="bg-dark navbar-dark ">
-            <div className="container-fluid d-flex px-5 justify-content-between">
-                <div style={{width:"clamp(2rem, 10vw, 7.5rem)"}} className="">
-                    <a><img src="/logo(highResolution).png" style={{maxWidth:"100%"}} alt="" /></a>
-                </div>
-                <ul className="d-flex  align-items-center">
-                    <li><a href=""><i class="fi fi-rr-home"></i></a></li>
-                    <li className="mx-5"><a href=""><i class="fi fi-rr-user"></i></a></li>
-                    <li><a href=""><i class="fi fi-rr-shopping-cart"></i></a></li>
-                </ul>
+      <>
+        <div className="container-fluid bg-dark" id="mobileLogo">
+            <div style={{ width: "clamp(2rem, 30vw, 7.5rem)" }}>
+              <a>
+                <img
+                  src="/logo(highResolution).png"
+                  style={{ maxWidth: "100%" }}
+                  alt=""
+                />
+              </a>
             </div>
+        </div>
+        <nav className="bg-dark navbar-dark " id="navigationBar">
+          <div className="container-fluid d-flex px-5 justify-content-between">
+            <div style={{ width: "clamp(2rem, 10vw, 7.5rem)" }} id="logoImage">
+              <a>
+                <img
+                  src="/logo(highResolution).png"
+                  style={{ maxWidth: "100%" }}
+                  alt=""
+                />
+              </a>
+            </div>
+            <ul className="d-flex  align-items-center" id="navigationContainer">
+              <li>
+                <a href="" title="Home Page">
+                  <i className="fi fi-rr-home"></i>
+                </a>
+              </li>
+              <li className="mx-5">
+                <a href="" title="Profile">
+                  <i className="fi fi-rr-user"></i>
+                </a>
+              </li>
+              <li>
+                <a href="" title="Cart">
+                  <i className="fi fi-rr-shopping-cart"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
         </nav>
+      </>
     );
   }
 }
